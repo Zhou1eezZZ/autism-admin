2995c383c43237011521c31268dcdae63cccdfe2
zhouli <563287128@qq.com>
2019-04-23 23:57:09 +0800

    init

---
+++ src/store/modules/user.js
@@ -0,0 +1,190 @@
+import { logout } from '@/api/login'
+import UserAPI from '@/api/user'
+import { getToken, removeToken } from '@/utils/auth'
+
+const user = {
+  state: {
+    token: getToken(),
+    name: '',
+    avatar: '',
+    roles: [],
+    isLogin: false,
+    address: '',
+    deptId: '',
+    email: '',
+    nation: '',
+    phone: '',
+    qualificationNumber: '',
+    realName: '',
+    relatedUser: [],
+    sex: '',
+    type: '',
+    uuid: '',
+    workTime: ''
+  },
+
+  mutations: {
+    SET_TOKEN: (state, token) => {
+      state.token = token
+    },
+    SET_NAME: (state, name) => {
+      state.name = name
+    },
+    SET_AVATAR: (state, avatar) => {
+      state.avatar = avatar
+    },
+    SET_ROLES: (state, roles) => {
+      state.roles = roles
+    },
+    SET_ISLOGIN: (state, isLogin) => {
+      state.isLogin = isLogin
+    },
+    SET_ADDRESS: (state, address) => {
+      state.address = address
+    },
+    SET_DEPTID: (state, deptId) => {
+      state.deptId = deptId
+    },
+    SET_EMAIL: (state, email) => {
+      state.email = email
+    },
+    SET_NATION: (state, nation) => {
+      state.nation = nation
+    },
+    SET_PHONE: (state, phone) => {
+      state.phone = phone
+    },
+    SET_QUALIFICATIONNUMBER: (state, qualificationNumber) => {
+      state.qualificationNumber = qualificationNumber
+    },
+    SET_REALNAME: (state, realName) => {
+      state.realName = realName
+    },
+    SET_RELATEDUSER: (state, relatedUser) => {
+      state.relatedUser = relatedUser
+    },
+    SET_SEX: (state, sex) => {
+      state.sex = sex
+    },
+    SET_TYPE: (state, type) => {
+      state.type = type
+    },
+    SET_UUID: (state, uuid) => {
+      state.uuid = uuid
+    },
+    SET_WORKTIME: (state, workTime) => {
+      state.workTime = workTime
+    },
+    SET_IP: (state, ip) => {
+      state.ip = ip
+    }
+  },
+
+  actions: {
+    // 登录
+    Login({ commit }, userInfo) {
+      return new Promise((resolve, reject) => {
+        UserAPI.userLogin(userInfo).then(response => {
+          if (response && response.data && response.data.successful) {
+            // debugger
+            const data = response.data.data
+            commit('SET_ADDRESS', data.address)
+            commit('SET_DEPTID', data.deptId)
+            commit('SET_EMAIL', data.email)
+            commit('SET_NAME', data.name)
+            commit('SET_NATION', data.nation)
+            commit('SET_PHONE', data.phone)
+            commit('SET_QUALIFICATIONNUMBER', data.qualificationNumber)
+            commit('SET_REALNAME', data.realName)
+            if (data.relatedUser) {
+              commit('SET_RELATEDUSER', data.relatedUser)
+            }
+            commit('SET_SEX', data.sex)
+            commit('SET_TYPE', data.type)
+            commit('SET_UUID', data.uuid)
+            commit('SET_WORKTIME', data.workTime)
+            commit('SET_ISLOGIN', true)
+          }
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 获取用户信息
+    GetInfo({ commit, userInfo }) {
+      return new Promise((resolve, reject) => {
+        UserAPI.userUpdate(userInfo).then(response => {
+          if (response && response.data && response.data.successful) {
+            const data = {
+              uuid: response.data.data
+            }
+            // debugger
+            UserAPI.getUserInfo(data).then(res => {
+              if (res && res.data && res.data.successful && res.data.data.list) {
+                const tmp = res.data.data.list[0]
+                commit('SET_ADDRESS', tmp.address)
+                commit('SET_DEPTID', tmp.deptId)
+                commit('SET_EMAIL', tmp.email)
+                commit('SET_NAME', tmp.name)
+                commit('SET_NATION', tmp.nation)
+                commit('SET_PHONE', tmp.phone)
+                commit('SET_QUALIFICATIONNUMBER', tmp.qualificationNumber)
+                commit('SET_REALNAME', tmp.realName)
+                if (tmp.relatedUser) {
+                  commit('SET_RELATEDUSER', tmp.relatedUser)
+                }
+                commit('SET_SEX', tmp.sex)
+                commit('SET_TYPE', tmp.type)
+                commit('SET_UUID', tmp.uuid)
+                commit('SET_WORKTIME', tmp.workTime)
+                commit('SET_ISLOGIN', true)
+              }
+              resolve(res)
+            }).catch(error => {
+              reject(error)
+            })
+          }
+        })
+      })
+    },
+
+    // 登出
+    LogOut({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        logout(state.token).then(() => {
+          commit('SET_TOKEN', '')
+          commit('SET_ROLES', [])
+          removeToken()
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 前端 登出
+    FedLogOut({ commit }) {
+      return new Promise(resolve => {
+        commit('SET_ADDRESS', '')
+        commit('SET_DEPTID', '')
+        commit('SET_EMAIL', '')
+        commit('SET_NAME', '')
+        commit('SET_NATION', '')
+        commit('SET_PHONE', '')
+        commit('SET_QUALIFICATIONNUMBER', '')
+        commit('SET_REALNAME', '')
+        commit('SET_RELATEDUSER', '')
+        commit('SET_SEX', '')
+        commit('SET_TYPE', '')
+        commit('SET_UUID', '')
+        commit('SET_WORKTIME', '')
+        commit('SET_ISLOGIN', false)
+        resolve()
+      })
+    }
+  }
+}
+
+export default user
---
+++ src/views/login/index.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="login-container">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+      <h3 class="title">自闭症儿童辅助干预后台管理系统</h3>
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input v-model="loginForm.phone" name="phone" type="text" auto-complete="on" placeholder="手机号" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          :type="pwdType"
+          v-model="loginForm.password"
+          name="password"
+          auto-complete="on"
+          placeholder="密码"
+          @keyup.enter.native="handleLogin" />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon :icon-class="pwdType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+      <el-form-item>
+        <el-button :loading="loading" type="primary" style="width:100%;" @click.native.prevent="handleLogin">
+          登 录
+        </el-button>
+      </el-form-item>
+      <div class="tips">
+        <!-- <span style="margin-right:20px;">username: admin</span>
+        <span> password: admin</span> -->
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        phone: '',
+        password: ''
+      },
+      loginRules: {
+        phone: [{ required: true, trigger: 'blur', pattern: /^[1][3,4,5,7,8][0-9]{9}$/, message: '请输入正确的手机号' }],
+        password: [{ required: true, trigger: 'blur', message: '请输入密码' }]
+      },
+      loading: false,
+      pwdType: 'password',
+      redirect: undefined
+    }
+  },
+  watch: {
+    $route: {
+      handler: function(route) {
+        this.redirect = route.query && route.query.redirect
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    showPwd() {
+      if (this.pwdType === 'password') {
+        this.pwdType = ''
+      } else {
+        this.pwdType = 'password'
+      }
+    },
+    handleLogin() {
+      this.loading = true
+      this.$store.dispatch('Login', this.loginForm).then(response => {
+        debugger
+        if (response && response.data && response.data.successful) {
+          this.$message({
+            type: 'success',
+            message: '登录成功',
+            duration: 1000
+          })
+          this.loading = false
+          this.$router.push({ path: this.redirect || '/' })
+          // this.$router.push({ path: '/dashboard' })
+        } else {
+          this.$message({
+            type: 'error',
+            message: response.data.statusMessage,
+            showClose: true
+          })
+        }
+      }).catch(() => {
+        this.loading = false
+      })
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+$bg:#2d3a4b;
+$light_gray:#eee;
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      &:-webkit-autofill {
+        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: #fff !important;
+      }
+    }
+  }
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+
+</style>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+.login-container {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background-color: $bg;
+  .login-form {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 520px;
+    max-width: 100%;
+    padding: 35px 35px 15px 35px;
+    margin: 120px auto;
+  }
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+  .title {
+    font-size: 26px;
+    font-weight: 400;
+    color: $light_gray;
+    margin: 0px auto 40px auto;
+    text-align: center;
+    font-weight: bold;
+  }
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
